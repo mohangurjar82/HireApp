1834e3a29a1604836cd7dc9c3387ae4f839c7773
razanjoshi <rajan4ualwz@hotmail.com>
2018-10-26 10:20:28 +0545

    Indentation fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-	has_one :pre_registration
+  has_one :pre_registration
 
-	class << self
-	  def from_omniauth(auth_hash)
-	  	auth_hash = auth_hash.as_json
-	    user = find_or_create_by(uuid:  auth_hash['uid'], provider: auth_hash['provider'])
-	    user.token = auth_hash['credentials']['token']
-	    user.email = auth_hash['info']['email']
-	    user.password = '12345678'
-	    user.save!
-	    user
-	  end
-	end
+  class << self
+    def from_omniauth(auth_hash)
+      auth_hash = auth_hash.as_json
+      user = find_or_create_by(uuid:  auth_hash['uid'], provider: auth_hash['provider'])
+      user.token = auth_hash['credentials']['token']
+      user.email = auth_hash['info']['email']
+      user.password = '12345678'
+      user.save!
+      user
+    end
+  end
 end
